604407f4ae423a0d8a5f43542844884af88edcd8
Jonathan W <aletwhittington@alets-air.fios-router.home>
2017-02-12 20:08:53 -0500

    first

--- First_Analysis.py
+++ First_Analysis.py
@@ -13,4 +13,4 @@ quandl.ApiConfig.api_key = "Vm3hGqA7K_chXo6DfTqx"
 
 mult_data = quandl.get(["WIKI/CHK.1", "WIKI/IBM.1"], start_date="2008-01-01", end_date="2015-12-31", collapse="monthly", transform="rdiff")
 
-mult_data.plot.hist(alpha=0.5)
+mult_data.plot.hist(alpha=0.3)
