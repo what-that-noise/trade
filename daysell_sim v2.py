bdac5f24516acdd2d6eb4a7f7ec82269730f8fbb
what-that-noise <jonathanrwhittington@outlook.com>
2017-03-03 17:05:20 -0500

    update

--- daysell_sim v2.py
+++ daysell_sim v2.py
@@ -10,7 +10,7 @@ def strat_sim(lngma, shtma, atr_cond, dayspass, symbol):
 import quandl
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 
 quandl.ApiConfig.api_key = "Vm3hGqA7K_chXo6DfTqx"
@@ -49,7 +49,7 @@ tck2.loc[tck2['rownum']>19, 'ATR'] = (tck2['TR']/20) + (tck2['ATR'].shift(1)*19/
 # Identify Buy Threshold by ATR
 atr_mult = 1.5
 tck2['buy threshold'] = tck2['openlongma'] + (tck2['ATR']*1.5)
-tck2['crossover buy'] = 0
+
 # Identify Buy
 tck2['buy'] = 0
 tck2 = tck2.dropna()
@@ -82,24 +82,46 @@ tck2['return'] = (tck2['Adj. Open']/tck2['Adj. Open'].shift(1)).fillna(1)
 tck2.loc[tck2['buy'] == 1, 'return'] = 1 # set start of each group to zero % return
 tck2['cumreturn'] = tck2.groupby('trade group')['return'].cumprod()
 
-# Calculate downside standard deviations
-tck2['negative returns'] = tck2['return'].map(lambda x: (x-1)**2 if x < 1 else 0)
 
-
-# Calculate strategy returns over time
+# Calculate cummulative return over strategy
 tck2['returns on trades'] = 1
 tck2.loc[tck2['trade group'] > 0 , 'returns on trades'] = tck2['return']
-tck2['cumreturn trades'] = tck2['returns on trades'].cumprod()
+tck2['cumreturn strategy'] = tck2['returns on trades'].cumprod()
+
+# Calculate Max Drawdowns from Trades
+tck2['cumreturn trade'] = tck2['returns on trades'].groupby(tck2['trade group']).cumprod()
+row_trades['diff'] = row_trades['sell row'] - row_trades['buy row']
+tck2['drawdown'] = 0
+for i in range(len(row_trades)):
+    grp_id = int(row_trades.iloc[i,1])
+    days = int(row_trades.iloc[i,4])
+    tck2.loc[tck2['trade group'] == grp_id, 'drawdown'] = (tck2['cumreturn trade'] - tck2['cumreturn trade'].rolling(days).max().shift(1)).fillna(0)
+
+tck2.drop(['cumreturn trade'], axis=1, inplace=True)
+
+# Calculate downside standard deviations
+tck2['negative returns sqrd'] = tck2['return'].map(lambda x: (x-1)**2 if x < 1 else 0)
+
 
 #summarize Results
+
+## Equity Curve
 investment = 1000
-tck2['equity curve'] = tck2['cumreturn trades'] * investment
-return_summary = pd.concat([tck2.groupby('trade group')['cumreturn'].last(), tck2.groupby('trade group')['cumreturn'].count(), ((251/tck2.groupby('trade group')['negative returns'].count())**0.5)*((tck2.groupby('trade group')['negative returns'].sum()/tck2.groupby('trade group')['negative returns'].count())**0.5)], axis=1)
-return_summary.columns = ['return','days', 'downside dev annulzd']
+equity_curve = tck2['cumreturn strategy'] * investment
+plt.plot(tck2['cumreturn strategy'])
+
+return_summary = pd.concat([tck2.groupby('trade group')['cumreturn'].last()-1,(tck2.groupby('trade group')['cumreturn'].last()**(251/tck2.groupby('trade group')['cumreturn'].count()))-1, tck2.groupby('trade group')['cumreturn'].count(), tck2.groupby('trade group')['negative returns sqrd'].sum()/tck2.groupby('trade group')['negative returns sqrd'].count()**0.5, ((251/tck2.groupby('trade group')['negative returns sqrd'].count())**0.5)*((tck2.groupby('trade group')['negative returns sqrd'].sum()/tck2.groupby('trade group')['negative returns sqrd'].count())**0.5), tck2.groupby('trade group')['drawdown'].max()], axis=1)
+return_summary.columns = ['rate of return on trade','rate of return for trade annlzd','days of trade', 'downside dev','downside dev annlzd', 'max drawdown']
 return_summary = return_summary.reset_index()
-return_summary['annualized return'] = (return_summary['return']**(251/return_summary['days'])) - 1
-return_summary['Sortino Ratio Anualzd'] = return_summary['annualized return']/return_summary['downside dev annulzd']
+return_summary['Sortino Ratio Anualzd'] = return_summary['rate of return for trade annlzd']/return_summary['downside dev annlzd']
+
 return_summary_filtered = return_summary.where((return_summary['trade group']>0) & (return_summary['days']>49) & (return_summary['annualized return']<10)).dropna()
-plt.hist(return_summary['annualized return'].where(return_summary['annualized return']<10).dropna(), bins=len(return_summary['annualized return'].where(return_summary['annualized return']<10).dropna()))
-plt.plot(tck2['equity curve'])
-#((251/tck2.groupby('trade group')['negative returns'].count())**0.5)*((tck2.groupby('trade group')['negative returns'].sum()/tck2.groupby('trade group')['negative returns'].count())**0.5)
+plt.hist(return_summary['rate of return for trade annlzd'].where(return_summary['rate of return for trade annlzd']<10).dropna(), bins=len(return_summary['rate of return for trade annlzd'].where(return_summary['rate of return for trade annlzd']<10).dropna()))
+# MAR = Average returns from Trades / Max Drawdown across all trades
+# R ratio = Sum of positive returns divided by sum of negative returns
+
+tck3 = tck2.reset_index()
+t = 365/((tck3['Date'].max() - tck3['Date'].min()).days)
+t_annual = 365/t
+strat_summary = pd.concat(pd.DataFrame([tck3['Date'].max(), tck3['Date'].min(), tck3['Date'].max()-tck3['Date'].min())], axis=1)
+[int(i.days) for i in (tck3['Date'].max() - tck3['Date'].min())]
